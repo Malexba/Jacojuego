2919282180c7fe830032ef1fc71613f8a566e758
casasvoley <acasaszan@gmail.com>
2021-07-14 20:02:47 +0200

    Add files via upload

---
+++ Cartas/Assets/Scripts/Datos/Cartas.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Carta")]
+public class Cartas : ScriptableObject
+{
+    public string nombre = "Nombre";
+
+}
