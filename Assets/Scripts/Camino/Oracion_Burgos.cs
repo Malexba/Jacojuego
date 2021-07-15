51bfe38cf5b4c42d7dcf0ea32d5dd57357d833f7
casasvoley <acasaszan@gmail.com>
2021-07-15 19:22:06 +0200

    Add files via upload

--- Assets/Scripts/Camino/Oracion_Burgos.cs
+++ Assets/Scripts/Camino/Oracion_Burgos.cs
@@ -2,22 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camino_Llano : MonoBehaviour
+public class Oracion_Burgos : MonoBehaviour
 {
-
-    // Moverse de 1 a 5 casillas
+    // +1 energía, +1 motivación, avanza una casilla
     public void ProvocarEfecto(){
         GameObject jugador = GameObject.FindGameObjectWithTag("Player");
-        jugador.GetComponent<Jugador_Mapa>().RestarEnergia(6); // Restar 6 puntos de energía
+        jugador.GetComponent<Jugador_Mapa>().RestarEnergia(2); // Restar 2 puntos de energía
+        jugador.GetComponent<Jugador_Mapa>().SumarMotivacion(1); // Sumar 1 punto de motivación
         GameObject casillaActual = jugador.GetComponent<Jugador_Mapa>().casillaActual;
         List<GameObject> casillasPosibles = new List<GameObject>();
         List<GameObject> auxs = new List<GameObject>();
-        // Encuentra las casillas a distancia de entre 1 y 5
-        for(int i =1;i < 6; i++){
-            auxs = casillaActual.GetComponent<Casilla_Mapa>().GetCasillasADistancia(i);
-            foreach(GameObject aux in auxs){
-                casillasPosibles.Add(aux);
-            }
+        // Encuentra las casillas a distancia de 1
+        auxs = casillaActual.GetComponent<Casilla_Mapa>().GetCasillasADistancia(1);
+        foreach(GameObject aux in auxs){
+            casillasPosibles.Add(aux);
         }
 
         // Activa el halo de todas ellas
