505146e289d911678c5ea292e11a53a3a46b5834
Malexba <80394779+Malexba@users.noreply.github.com>
2021-07-16 00:34:59 +0200

    Add files via upload

--- Assets/Scripts/Intermedio/Mochila.cs
+++ Assets/Scripts/Intermedio/Mochila.cs
@@ -13,11 +13,16 @@ public class Mochila : MonoBehaviour // En verdad esto es el gestor de toda la e
     public GameObject abajo;
     public GameObject[] carta;
     public Image astro;
+    public Image fondo;
+    public Image[] interactuable;
     public Sprite[] astros;
+    public Sprite[] fondos;
     public Text nombre;
+    public Text boton;
     public Image img;
     public Text texto;
     public Jugador jugador;
+    public Character[] personajes;
     bool mochi;
     private int posLista = 0; // Posición en la lista del mazo (mod 3)
 
@@ -25,10 +30,22 @@ public class Mochila : MonoBehaviour // En verdad esto es el gestor de toda la e
     {
         if (jugador.inicio)
         {
+            boton.text = "Comencemos";
+            fondo.sprite = fondos[0];
             astro.sprite = astros[0];
+            interactuable[3].sprite = personajes[3].img;
+            interactuable[4].sprite = personajes[4].img;
         } else
         {
+            boton.text = "A dormir";
+            fondo.sprite = fondos[1];
             astro.sprite = astros[1];
+            interactuable[3].sprite = personajes[3].img2;
+            interactuable[4].sprite = personajes[4].img2;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            interactuable[i].sprite = personajes[i].img;
         }
     }
 
